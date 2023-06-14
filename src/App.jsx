120036009480ac8b342b63672f13b41f07c47e2c
jamaal <palacebikes@gmail.com>
2023-06-14 03:48:06 -0700

    added function to show puppies and their info, added css styling as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import './index.css'
+
 
 function App() {
 
-  return (
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-  )
-}
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null); 
 
-export default App
+  function handleClick(e) {
+    const clickedPuppy = e.target.innerText;
+    const clickedPuppyId = puppyList.find(puppy => puppy.name === clickedPuppy).id;
+    setFeatPupId(clickedPuppyId);
+  } 
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+
+  console.log(featuredPup);
+
+  return (
+    <div className="App">
+      <div className='puppy-container'>
+      {featPupId && (
+        <div className='puppy-card'>
+          <div className='puppy-info'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+          </div>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    <img src="https://cdn.discordapp.com/attachments/985476606137073734/1118459315662094346/iu.png" className="logo react" alt="Different logo" />
+    </div>
+    </div>
+
+  );
+}
+export default App;
